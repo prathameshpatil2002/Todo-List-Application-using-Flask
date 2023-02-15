747ed72d7d846e11a448b39bcf6160a6ad8b6511
Prathamesh patil <73377212+prathameshpatil2002@users.noreply.github.com>
2023-02-15 20:53:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# SQLite Configuration
+# SQLite Configuration  '///' - relative path
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
